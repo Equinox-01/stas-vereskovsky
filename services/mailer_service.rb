223da7b805b7df154d4f3067222c4d6da90af0ea
Stas Vereskovsky <vereskovskystas@gmail.com>
2020-07-04 20:16:19 +0300

    Add mailer for notifications

---
+++ services/mailer_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'pony'
+
+class MailerService
+  class << self
+    def send_notification(report)
+      if ENV['USER_NAME'] && ENV['PASS'] && ENV['DESTINATION']
+        Pony.mail to: ENV['DESTINATION'],
+                  from: ENV['USER_NAME'],
+                  subject: 'CV Notification',
+                  body: report,
+                  via: :smtp,
+                  via_options: {
+                    address: 'smtp.gmail.com',
+                    port: '587',
+                    user_name: ENV['USER_NAME'],
+                    password: ENV['PASS'],
+                    authentication: :plain
+                  }
+        puts report
+      else
+        puts 'Critical error: No credentials for mailer'
+      end
+    end
+  end
+end
